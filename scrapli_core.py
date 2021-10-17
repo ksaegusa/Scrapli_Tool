7fc3f193ed4139e240c809ef21ea852c6b3853fa
ksaegusa <snack11monster@gmail.com>
2021-10-17 22:27:07 +0900

    add future

--- scrapli_core.py
+++ scrapli_core.py
@@ -1,65 +1,72 @@
-import re
+
 import logging
 
 from scrapli import Scrapli
-from dataclasses import dataclass
+import q
+#logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
 
-# logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
-
-@dataclass
-class COMMAND_LIST:
-    commands: list
 
 def login(config):
     try:
         connection = Scrapli(**config)
         connection.open()
-        error_flag=False
+        error_flag = False
     except Exception as e:
         print(f'Login Error!: {e}')
         error_flag = True
         connection = ''
     return connection, error_flag, config['host']
 
+
 def logout(connect):
     connect.close()
 
-def send_command(connection, commands, host):
+
+# TODO: 実行結果にエラーがないか調べる機能も足したい
+def send_command(connection, commands):
     try:
         output_list = list()
         if commands:
             for command in commands:
                 res_command = connection.send_command(command=command)
                 output_list.append(res_command.result)
+        return output_list
     except Exception as e:
         print(f'Send Commnad Error!: {command} >> {e}')
-    responce_command_to_text(host, commands, output_list)
 
-# TODO: Error処理書く
-def responce_command_to_text(host, commands,output_list):
-    filename = f"output/{host}.log"
+
+def responce_command_to_text(device_name, commands, output_list):
+    filename = f"output/{device_name}.log"
     with open(filename, mode='a') as f:
-        for co, re in zip(commands,output_list):
+        for co, re in zip(commands, output_list):
             f.write("="*15 + co + "="*15 + "\n")
             f.write(f"{re}\n\n")
     print(f"* CREATE => {filename}")
 
-# NOTE: ConfigureReplaceコマンドの実装
+
 def configure_replace_to_startup(connection):
     try:
         connection.send_command(command='configure replace nvram:startup-config list force')
     except Exception as e:
         print(f'Configure Replace Error!: {e}')
 
+
 # スレッド処理の実行部分
-# HACK: コマンドをここに書いてるのがイケてない
-def _run(config):
-    net,error_flag, host = login(config)
+def _run(config, commands):
+    device = {
+        "host": config['host'],
+        "auth_username": config['auth_username'],
+        "auth_password": config['auth_password'],
+        "auth_strict_key": False,
+        "platform": config['platform'],
+        }
+
+    net, error_flag, host = login(device)
     try:
         if not error_flag:
-            commands = [
-                "show rwun",
-            ]
-            send_command(net, commands, host)
+            res_command = send_command(net, commands)
+            responce_command_to_text(config['device_name'],
+                                     commands,
+                                     res_command)
     finally:
         logout(net)
--- scrapli_main.py
+++ scrapli_main.py
@@ -4,24 +4,33 @@
 import argparse
 import yaml
 import concurrent.futures
-
 import scrapli_core
 
 from timeit import default_timer as timer
 
+
 def main():
     start = timer()
     parser = argparse.ArgumentParser(prog='SSH Tool')
-    parser.add_argument('-i','--inventory', dest='inventory_file', help='Please select a file in Yaml format')
-    parser.add_argument('-f','--fork', dest='fork', help='forks num', type=int, default=5)
+    parser.add_argument('-i', '--inventory', dest='inventory_file',
+                        help='Please select a file in Yaml format')
+    parser.add_argument('-f', '--fork', dest='fork', help='forks num',
+                        type=int, default=5)
+    parser.add_argument('-c', '--command', dest='command', help='command list')
     args = parser.parse_args()
 
-    print(f"==== START ====")
-    with open(args.inventory_file, 'r') as yml:
-        configs = yaml.safe_load(yml)
-        with concurrent.futures.ProcessPoolExecutor(max_workers=args.fork) as excuter:
-            excuter.map(scrapli_core._run, configs)
-    print(f"==== END ====")
+    # TODO: inputの確認処理がない
+    print("==== START ====")
+    with open(args.inventory_file, 'r') as inventory_yml:
+        configs = yaml.safe_load(inventory_yml)
+        commands = list()
+        with open(args.command, 'r') as commands_yml:
+            command = yaml.safe_load(commands_yml)
+            for i in range(0, len(configs)):
+                commands.append(command[configs[i]['platform']])
+            with concurrent.futures.ProcessPoolExecutor(max_workers=args.fork) as excuter:
+                excuter.map(scrapli_core._run, configs, commands)
+    print("==== END ====")
 
     end = timer()
     print("#"*70)
